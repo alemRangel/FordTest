54d36d85000795ecfc31ae0e1992e1e0eb02db44
Alejandro Rangel <buffalem@gmail.com>
2024-01-31 17:45:50 -0600

    Gets to 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int testValue = 5;
+        int testValue = 6;
         RomanNumerals romanNumerals = new RomanNumerals(testValue);
         System.out.println(romanNumerals.getRoman());
     }
--- src/com/company/RomanNumerals.java
+++ src/com/company/RomanNumerals.java
@@ -23,7 +23,19 @@ public class RomanNumerals {
         String partialRoman = "";
         if(map.containsKey(numToTranslate)){
             partialRoman = map.get(numToTranslate);
+        } else {
+            if(numToTranslate >= 2 && numToTranslate <4){
+                partialRoman = "I";
+                for(int i=0; i<numToTranslate-1; ++i){
+                    partialRoman = partialRoman + "I";
+                }
+            } else {
+                if(numToTranslate == 4){
+                    partialRoman = "IV";
+                }
+            }
         }
+
         return partialRoman;
     }
 }
